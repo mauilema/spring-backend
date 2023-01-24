61fb3ceb87a2a0bfa5d95d690d8fc62bf0eb57a0
mauilema <srjorgemaciel2002@gmail.com>
2023-01-23 22:01:59 -0500

    adding the aws s3 upload logic

--- src/main/java/com/example/springbackend/resource/FileResource.java
+++ src/main/java/com/example/springbackend/resource/FileResource.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping
+@RequestMapping("api/files")
 public class FileResource {
 
     @Autowired
--- src/main/java/com/example/springbackend/service/FileService.java
+++ src/main/java/com/example/springbackend/service/FileService.java
@@ -1,11 +1,29 @@
 package com.example.springbackend.service;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 public class FileService {
     public boolean upload(MultipartFile file) {
-        return true;
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_2).build();
+        try {
+
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(file.getSize());
+            metadata.setContentType(file.getContentType());
+            s3.putObject("backend-winter23", file.getOriginalFilename(), file.getInputStream(), metadata);
+            return true;
+        } catch (AmazonServiceException | IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
