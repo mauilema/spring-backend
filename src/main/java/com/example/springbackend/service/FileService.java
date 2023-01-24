361c780f0d832407c47c5fb8e2f758aee09ce192
mauilema <srjorgemaciel2002@gmail.com>
2023-01-23 21:08:32 -0500

    creating a service and resource class to use alongside AWS S3 upload service

---
+++ src/main/java/com/example/springbackend/service/FileService.java
@@ -0,0 +1,11 @@
+package com.example.springbackend.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileService {
+    public boolean upload(MultipartFile file) {
+        return true;
+    }
+}
