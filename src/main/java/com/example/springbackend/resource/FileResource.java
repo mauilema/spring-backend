95d139a75a585cd74e04c3b53e7152d3ef3154cf
mauilema <srjorgemaciel2002@gmail.com>
2023-01-25 16:45:42 -0500

    creating get and delete logic in service class, and creating get endpoint to view aws s3 file

--- src/main/java/com/example/springbackend/resource/FileResource.java
+++ src/main/java/com/example/springbackend/resource/FileResource.java
@@ -1,12 +1,13 @@
 package com.example.springbackend.resource;
 
+import com.amazonaws.services.s3.model.S3Object;
 import com.example.springbackend.service.FileService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("api/files")
@@ -18,5 +19,12 @@ public class FileResource {
     @PostMapping
     public boolean upload(@RequestParam(name = "file") MultipartFile file) {
         return fileService.upload(file);
+    }
+
+    @GetMapping("/view")
+    public void view(@RequestParam(name = "key") String key, HttpServletResponse response) throws IOException {
+        S3Object object = fileService.getFile(key);
+        response.setContentType(object.getObjectMetadata().getContentType());
+        response.getOutputStream().write(object.getObjectContent().readAllBytes());
     }
 }
--- src/main/java/com/example/springbackend/service/FileService.java
+++ src/main/java/com/example/springbackend/service/FileService.java
@@ -8,6 +8,7 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -29,5 +30,17 @@ public class FileService {
             e.printStackTrace();
             return false;
         }
+    }
+
+    public S3Object getFile(String key) {
+        BasicAWSCredentials credentials = new BasicAWSCredentials(System.getenv("AWS_ACCESS_KEY_ID"), System.getenv("AWS_SECRET_ACCESS_KEY"));
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(Regions.US_EAST_2).build();
+        return s3.getObject(System.getenv("AWS_S3_BUCKET_NAME"), key);
+    }
+
+    public void deleteFile(String key) {
+        BasicAWSCredentials credentials = new BasicAWSCredentials(System.getenv("AWS_ACCESS_KEY_ID"), System.getenv("AWS_SECRET_ACCESS_KEY"));
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(Regions.US_EAST_2).build();
+        s3.deleteObject(System.getenv("AWS_S3_BUCKET_NAME"), key);
     }
 }
