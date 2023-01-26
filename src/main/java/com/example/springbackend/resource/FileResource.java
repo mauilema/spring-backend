ae1d1003e4e09e64962004020664e13c9d453a5e
mauilema <srjorgemaciel2002@gmail.com>
2023-01-25 19:17:07 -0500

    creating a get endpoint to download an aws s3 photo

--- src/main/java/com/example/springbackend/resource/FileResource.java
+++ src/main/java/com/example/springbackend/resource/FileResource.java
@@ -1,9 +1,14 @@
 package com.example.springbackend.resource;
 
+import com.amazonaws.services.s3.Headers;
 import com.amazonaws.services.s3.model.S3Object;
 import com.example.springbackend.service.FileService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -27,4 +32,14 @@ public class FileResource {
         response.setContentType(object.getObjectMetadata().getContentType());
         response.getOutputStream().write(object.getObjectContent().readAllBytes());
     }
+
+    @GetMapping("/download")
+    public ResponseEntity<Resource> download(@RequestParam(name = "key") String key) throws IOException {
+        S3Object object = fileService.getFile(key);
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(object.getObjectMetadata().getContentType()))
+                .header(Headers.CONTENT_DISPOSITION, "attachment; filename=\"" + key + "\"")
+                .body(new ByteArrayResource(object.getObjectContent().readAllBytes()));
+    }
+
 }
