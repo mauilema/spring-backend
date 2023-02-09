5044153829cf04e7bd4a8b1480a4b63fa940b942
mauilema <srjorgemaciel2002@gmail.com>
2023-02-08 23:10:37 -0500

    using a valid id token, created config package with configuration class alongside a bean method, fixes Firebase name [DEFAULT] already exists error

--- src/main/java/com/example/springbackend/service/FirebaseService.java
+++ src/main/java/com/example/springbackend/service/FirebaseService.java
@@ -1,39 +1,18 @@
 package com.example.springbackend.service;
 
 import com.example.springbackend.model.FirebaseUser;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Service;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 
 @Service
 public class FirebaseService {
 
-    public FirebaseApp initializeFirebase() throws IOException {
-
-        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        Resource resource = resolver.getResource("classpath:backend-winter23-firebase.json");
-
-        FileInputStream serviceAccount = new FileInputStream(resource.getFile());
-
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://backend-winter23.firebaseio.com")
-                .build();
-
-        return FirebaseApp.initializeApp(options);
-    }
-
     public FirebaseUser authenticate(String idToken) throws IOException, FirebaseAuthException {
 
-        String uid = FirebaseAuth.getInstance(initializeFirebase()).verifyIdToken(idToken).getUid();
+        String uid = FirebaseAuth.getInstance().verifyIdToken(idToken).getUid();
         String name = FirebaseAuth.getInstance().verifyIdToken(idToken).getName();
         String email = FirebaseAuth.getInstance().verifyIdToken(idToken).getEmail();
 
